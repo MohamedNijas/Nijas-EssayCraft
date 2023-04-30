2e022d3fa52e45a5827501c08dbef7f4319f5512
MohamedNijas <93792336+MohamedNijas@users.noreply.github.com>
2023-05-01 00:21:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-#from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain,SequentialChain
@@ -8,12 +7,11 @@ from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper
 
 api_key = os.environ.get('OPENAI_API_KEY')
-#os.environ["OPENAI_API_KEY"] = apikey
-#memory
+
 
 title_memory = ConversationBufferMemory(input_key = "topic",memory_key = "chat_history" )
 script_memory = ConversationBufferMemory(input_key = "title",memory_key = "chat_history")
-#APP
+
 
 st.title("Nijas EssayCraft")
 st.subheader("Only enter the keyword.I will give the title and Article for you")
@@ -21,7 +19,7 @@ prompt = st.text_input("Enter your keyword")
 
 submitted = st.button("submit")
 
-#prompt templates
+
 title_template =  PromptTemplate(
     input_variables = ["topic"],
     template = "give me the title to write a Article for a webpage on this topic: {topic}"
@@ -35,16 +33,15 @@ llm = OpenAI(temperature = 0.9)
 
 title_chain = LLMChain(llm = llm,prompt = title_template,verbose = True,output_key = "title",memory = title_memory )
 script_chain = LLMChain(llm = llm,prompt = script_template,verbose = True,output_key = "script",memory = script_memory)
-#sequential_chain = SequentialChain(chains = [title_chain,script_chain],input_variables = ["topic"],output_variables= ["title","script"],verbose = True)
-#show the prompt
+
+
 wiki = WikipediaAPIWrapper()
 if prompt and submitted:
-    #response = sequential_chain({"topic":prompt})
+    
     title = title_chain.run(prompt )
     wiki_research = wiki.run(prompt)
     script = script_chain.run(title = title,wikipedia_research = wiki_research)
-    #st.write(response["title"])
-    #st.write(response["script"])
+   
     st.write(title)
     st.write(script)
 
