8d4acff5cbc6c11e4f8e5260b364c711732df460
MohamedNijas <93792336+MohamedNijas@users.noreply.github.com>
2023-04-30 23:37:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ script_memory = ConversationBufferMemory(input_key = "title",memory_key = "chat_
 page_bg_img = '''
 <style>
 body {
-background-image: url("https://dms.uom.lk/s/mcZXCLp3NASrpKt");
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
 background-size: cover;
 }
 </style>
